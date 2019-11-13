591194effadb888dca57e35829bb56d324fe5cee
Simon Giraudot <sosage@ptilouk.net>
2019-11-13 11:17:06 +0100

    Better errors

--- include/Sosage/Utils/error.h
+++ include/Sosage/Utils/error.h
@@ -6,22 +6,23 @@
 namespace Sosage
 {
 
-#if defined(NDEBUG)
-#define check(test, msg) (static_cast<void>(0))
-#else
 #define check(test, msg) if (!(test)) check_impl (__FILE__, __LINE__, msg)
+
+#if defined(NDEBUG)
+#define dbg_check(test, msg) (static_cast<void>(0))
+#else
+#define dbg_check(test, msg) if (!(test)) check_impl (__FILE__, __LINE__, msg)
 #endif
 
 #if defined(SOSAGE_ASSERTIONS_AS_EXCEPTIONS)
 inline void check_impl (const char* file, int line, const std::string& str)
 {
-  throw std::runtime_error(str + " (" + file + ":" + std::to_string(line));
+  throw std::runtime_error(str + " [" + file + ":" + std::to_string(line) + "]");
 }
 #else
 inline void check_impl (const char* file, int line, const std::string& str)
 {
-  std::cerr << "Error: " << file << ":" << line << std::endl
-            << "Error: "<< str << std::endl;
+  std::cerr << "Error: "<< str << " [" << file << ":" << line << "]" << std::endl;
   abort();
 }
 #endif
