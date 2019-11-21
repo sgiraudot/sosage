92568d59fde37d45f2bcf92cfc1f3865550ba826
Simon Giraudot <simon.giraudot@geometryfactory.com>
2019-11-21 15:10:00 +0100

    Touch events + more cleaning

--- include/Sosage/Utils/vector_2.h
+++ include/Sosage/Utils/vector_2.h
@@ -1,7 +1,7 @@
 #ifndef SOSAGE_UTILS_VECTOR_2_H
 #define SOSAGE_UTILS_VECTOR_2_H
 
-#include <cassert>
+#include <Sosage/Utils/error.h>
 #include <vector>
 
 namespace Sosage
@@ -10,34 +10,9 @@ namespace Sosage
 template <typename T>
 class vector_2
 {
-  std::vector<std::vector<T> > m_data;
-
-public:
-
-  vector_2 (std::size_t width = 0, std::size_t height = 0, const T& def = T())
-  {
-    m_data.resize(width);
-    for (std::size_t i = 0; i < width; ++ i)
-      m_data[i].resize(height, def);
-  }
-
-  std::size_t width() const { return m_data.size(); }
-  std::size_t height() const { return m_data.front().size(); }
-
-  const T& operator() (std::size_t x, std::size_t y) const
-  {
-    assert (x < width() && y < height());
-    return m_data[x][y];
-  }
-  T& operator() (std::size_t x, std::size_t y)
-  {
-    assert (x < width() && y < height());
-    return m_data[x][y];
-  }
-#if 0
   std::vector<T> m_data;
   std::size_t m_width;
-
+  
 public:
 
   vector_2 (std::size_t width = 0, std::size_t height = 0, const T& def = T())
@@ -49,15 +24,18 @@ public:
 
   const T& operator() (std::size_t x, std::size_t y) const
   {
-    assert (x + m_width * y < m_data.size());
+    dbg_check (x + m_width * y < m_data.size(), "Access to vector_2["
+               + std::to_string(x) + ";" + std::to_string(y) + "] out of bounds ["
+               + std::to_string(width()) + "; " + std::to_string(height()));
     return m_data[x + m_width * y];
   }
   T& operator() (std::size_t x, std::size_t y)
   {
-    assert (x + m_width * y < m_data.size());
+    dbg_check (x + m_width * y < m_data.size(), "Access to vector_2["
+               + std::to_string(x) + ";" + std::to_string(y) + "] out of bounds ["
+               + std::to_string(width()) + "; " + std::to_string(height()));
     return m_data[x + m_width * y];
   }
-#endif
 };
 
 } // namespace Sosage
