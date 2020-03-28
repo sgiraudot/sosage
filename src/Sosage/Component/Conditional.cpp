72ce6a18abb57eb14cc8c2a8c554917d5387cdff
Simon Giraudot <sosage@ptilouk.net>
2020-03-28 15:26:51 +0100

    Default actions

--- src/Sosage/Component/Conditional.cpp
+++ src/Sosage/Component/Conditional.cpp
@@ -4,14 +4,14 @@ namespace Sosage::Component
 {
 
 Variable::Variable (const std::string& id, Handle target)
-  : Base(id), m_target (target)
+  : Conditional_base(id), m_target (target)
 { }
 
 Conditional::Conditional (const std::string& id,
                           Condition_handle condition,
                           Handle if_true,
                           Handle if_false)
-  : Base(id)
+  : Conditional_base(id)
   , m_condition (condition)
   , m_if_true (if_true)
   , m_if_false (if_false)
@@ -32,14 +32,14 @@ std::string Conditional::str() const
     + ift + " : " + iff;
 }
 
-Handle Conditional::get()
+Handle Conditional::get() const
 {
   return (m_condition->value() ? m_if_true : m_if_false);
 }
 
 State_conditional::State_conditional (const std::string& id,
                                       State_handle state)
-  : Base(id)
+  : Conditional_base(id)
   , m_state (state)
 { }
 
@@ -68,6 +68,42 @@ Handle State_conditional::get() const
     return Handle();
 //  check (iter != m_handles.end(), "Cannot find state " + m_state->value() + " in " + id());
   return iter->second;
+}
+
+Random_conditional::Random_conditional (const std::string& id)
+  : Conditional_base(id)
+  , m_total(0)
+{ }
+
+Random_conditional::~Random_conditional()
+{
+  m_handles.clear();
+}
+
+std::string Random_conditional::str() const
+{
+  return this->id() + " -> " + get()->id();
+}
+
+void Random_conditional::add (double probability, Handle h)
+{
+  m_handles.push_back (std::make_pair (probability, h));
+  m_total += probability;
+}
+  
+Handle Random_conditional::get() const
+{
+  double random = m_total * (rand() / double(RAND_MAX));
+  double accu = 0;
+
+  std::size_t idx = 0;
+  while (random >= accu && idx < m_handles.size())
+  {
+    accu += m_handles[idx].first;
+    ++ idx;
+  }
+
+  return m_handles[idx-1].second;
 }
 
 
